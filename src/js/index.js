c35790e912729328e5a7a3e06c1a1f8a5d411c77
BesmellahBassam <BesmellahBasam@gmail.com>
2022-09-05 00:07:18 +0430

    loading recipes from the api and showing them to the user

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -8,37 +9,35 @@ import Search from './models/Search';
  * - liked recipes
  */
 
+const state = {}
 
-const state = {
+const constrolSearch = async () => {
 
-}
-
-const constrolSearch = () => {
     // 1) Get the query from view 
-  const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query);
 
   if(query) {
+    
     // 2) New search object and add it to the state
     state.search = new Search(query);
-    // 3) Prepare the UI for results
 
+    // 3) Prepare the UI for results
+    searchView.clearInputs();
+    searchView.clearResults();
+    
     // 4) Search for recipes
-    state.search.getResults();
+    await state.search.getResults();
 
     // 5) Render results on UI
-
-
+    console.log(state.search.result);
+    searchView.renderRecipes(state.search.result);
   }
   
 }
 
-document.querySelector('.search').addEventListener('submit', (e) => {
+elements.searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   constrolSearch();
 
 })
-
-const search = new Search('pizza');
-search.getResults();
-// const data = await search.getResults();
-console.log(search);
